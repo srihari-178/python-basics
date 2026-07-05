b0d361ec18b31edf923bce18039510d5cfd31ba9
SriHari Avula <its.srihari.avula@gmail.com>
2026-07-05 14:11:33 +0530

    basics of python

---
+++ variables.py
@@ -0,0 +1,22 @@
+#variables
+a = 10
+num_servers = 5
+# datatypes
+#1. Number
+a = 10
+print(type(a))     #Integer
+
+CONST_PI=3.14
+print(CONST_PI)
+print(type(CONST_PI))
+CONST_PI= 3.15         #Float
+print(CONST_PI)
+
+#2.String
+
+server = "Ansible"  # String
+print(type(server))
+
+#3.Boolean 
+isProd = False
+print(type(isProd))
