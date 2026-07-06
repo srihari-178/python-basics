8992b73ab637a06ca28ba896762b46bf31a705db
SriHari Avula <its.srihari.avula@gmail.com>
2026-07-07 01:25:32 +0530

    loops_test

--- functions.py
+++ functions.py
@@ -1,23 +1,26 @@
 def greet():
   print("hello")
 
-greet()
+# greet()
 
 def greet(name):
   print("hello",name)
 
-greet("hemanth")
+# greet("hemanth")
 
 def add(num1,num2):
   return num1+num2
 sum = add(134,342)
-print(sum)
+# print(sum)
 
 # positional arguments -> you can pass multiple positional arguments by using *args 
 # keyword arguments -> you can pass multiple keyword arguments by using **kwargs
 
 def mult(num1,num2,*args,**kwargs):
+  print(args,kwargs)
   return num1*num2
 
 result = mult(45,6,837,3648,name="Srihari",value="zero")
 print(result)
+
+print(r"https://learn.joindevops.com/learn/home/DevSecOps-with-AWS-86S/section/664822/lesson/4534653?")
